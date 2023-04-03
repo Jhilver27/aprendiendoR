17d4fffdc25f87bf21f91e9524e1ddbb00b4e037
jhilver <jhilver27@gmail.com>
2023-04-03 08:58:33 -0500

    aplicar formato tipo fecha a TIME

--- aprendiendo_R/tasa_interes.R
+++ aprendiendo_R/tasa_interes.R
@@ -4,15 +4,19 @@ library(tidyverse)
 library(ggplot2)
 library(scales)
 library(plotly)
+library(zoo)
 
 # leer bases de datos
 datos <- read.csv("tasaInteresMes.csv",header = TRUE ,sep = ",")
 View(datos)
+# Formatear tipo fecha columna TIME
+datos$Fecha <- as.Date(as.yearmon(datos$TIME))
 
-grafico1 <- ggplot(datos, aes(x = TIME, y =Value/100 , group = LOCATION, color = LOCATION)) +
+grafico1 <- ggplot(datos, aes(x = Fecha, y =Value/100 , group = LOCATION, color = LOCATION)) +
   geom_line()+
   labs(x = "Fecha", y ="Tasa de interés", color = "País")+
   scale_y_continuous(labels = percent )
+
 ggplotly(grafico1)
 
   
